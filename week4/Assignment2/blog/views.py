ec443064642b63caa1906dd8741479c8dc753692
pouria <bapouria@gmail.com>
2024-10-04 06:50:35 -0400

    Search functionality added

--- week4/Assignment2/blog/views.py
+++ week4/Assignment2/blog/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Student
 from .forms import StudentForm
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q  # For complex queries
 
 
 # Create your views here.
 
 def student_list(request):
-    students = Student.objects.all()
+    query = request.GET.get('q')  # Get the search query
+    if query:
+        # Filter students by first name or last name containing the search query
+        students = Student.objects.filter(
+            Q(first_name__icontains=query) | Q(last_name__icontains=query)
+        )
+    else:
+        students = Student.objects.all()  # Show all students if no search query
+
     context = {"students": students}
     return render(request, "blog/student_list.html", context)
 
