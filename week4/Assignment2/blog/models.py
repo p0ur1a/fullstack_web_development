80acf16cace75787d3534ed26c780ca4ab9328fb
pouria <bapouria@gmail.com>
2024-10-04 06:39:52 -0400

    Email and grade verification added

--- week4/Assignment2/blog/models.py
+++ week4/Assignment2/blog/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from django.core.validators import EmailValidator, MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(
+        max_length=254, 
+        validators=[EmailValidator(message="Enter a valid email address.")]
+    )
     date_of_birth = models.DateField()
     enrolled = models.DateField()
-    grade = models.IntegerField()
-    
+    grade = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(12)]
+    )
+        
     class Meta:
         verbose_name_plural = "Students"
         
