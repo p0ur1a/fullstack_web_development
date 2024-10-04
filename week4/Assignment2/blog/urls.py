273614bdfbd5436ea7cae4714c463635b5b21028
pouria <bapouria@gmail.com>
2024-10-03 23:09:00 -0400

    all functionalities up to point 4 added

--- week4/Assignment2/blog/urls.py
+++ week4/Assignment2/blog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # path("", views.student_list, name="student_list"),
 
     path('', views.student_list, name='student_list'),
-
+    path('students/<int:student_id>/', views.student_detail, name='student_detail'),
+    path('students/add/', views.add_student, name='add_student'),
+    path('students/<int:student_id>/edit/', views.edit_student, name='edit_student'),
 ]
--- week4/Assignment2/blog/views.py
+++ week4/Assignment2/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Student
+from .forms import StudentForm
 
 # Create your views here.
 
@@ -8,7 +9,32 @@ def student_list(request):
     context = {"students": students}
     return render(request, "blog/student_list.html", context)
 
-# def student_detail(request, student_id):
-#     student = get_object_or_404(Student, id=student_id)
-#     context = {"student": student}
-#     return render(request, "blog/student_detail.html", context)
+def student_detail(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    context = {"student": student}
+    return render(request, "blog/student_detail.html", context)
+
+
+def add_student(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    
+    return render(request, 'blog/add_student.html', {'form': form})
+
+def edit_student(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+    
+    return render(request, 'blog/edit_student.html', {'form': form, 'student': student})
